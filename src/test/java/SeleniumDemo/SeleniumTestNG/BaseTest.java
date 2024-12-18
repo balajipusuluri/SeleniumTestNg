da05ea23600b9a396c1a99a7ee0ddd28f9bccf64
balajipusuluri <37187293+balajipusuluri@users.noreply.github.com>
2024-12-18 14:19:12 +0530

    BaseTest.java

--- src/test/java/SeleniumDemo/SeleniumTestNG/BaseTest.java
+++ src/test/java/SeleniumDemo/SeleniumTestNG/BaseTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public  class BaseTest {
 
